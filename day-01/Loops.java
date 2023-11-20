74d5c3afc50d8b1a8a9c9fbd343cd198713eade9
Yashwant Das <yashworks@gmail.com>
2023-11-20 16:02:55 +0530

    update Hello, Loops, Switch

---
+++ day-01/Loops.java
@@ -0,0 +1,14 @@
+/**
+ * Loops
+ */
+public class Loops {
+
+    public static void main(String[] args) {
+    // loop - while, do while, for
+    int i = 1;
+    while (i < 5) {
+        System.out.println("Value of i is: " +i);  
+        i++; 
+    }
+    }
+}
---
+++ day-01/Switch.java
@@ -0,0 +1,27 @@
+public class Switch {
+    public static void main(String[] args) {
+        String day = "Monday";
+        String result = "";
+        // switch 1
+        // switch (day) {
+        //     case "Sunday", "Saturday":
+        //         System.out.println("Weeknends");
+        //         break;
+        //     case "Monday":
+        //         System.out.println("Weekdays");
+        //         break;
+        //     default:
+        //         System.out.println("Not sure");
+        //         break;
+        // }
+
+        // switch 2
+        result = switch(day)
+        {
+            case "Monday" -> "8am";
+            case "Saturday" -> "6am";
+            default -> "7am";
+        };
+        System.out.println(result);
+    }
+}
