51651a40a127239d9ccd9fa27a50b43b3b8d00fd
Yashwant Das <yashworks@gmail.com>
2023-11-20 23:03:08 +0530

    update Hello.java, Loops.java and Switch.java update Hello.java

--- day-01/Loops.java
+++ day-01/Loops.java
@@ -3,12 +3,12 @@
  */
 public class Loops {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
     // loop - while, do while, for
     int i = 1;
     while (i < 5) {
-        System.out.println("Value of i is: " +i);  
-        i++; 
+      System.out.println("Value of i is: " + i);
+      i++;
     }
-    }
+  }
 }
