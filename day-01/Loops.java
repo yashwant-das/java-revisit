1e879c84080ee0d4d520bf6a590a1027a118f9e0
Yashwant Das <yashworks@gmail.com>
2023-11-21 07:02:58 +0530

    update Loops.java

--- day-01/Loops.java
+++ day-01/Loops.java
@@ -6,9 +6,15 @@ public class Loops {
   public static void main(String[] args) {
     // loop - while, do while, for
     int i = 1;
+    // while
     while (i < 5) {
       System.out.println("Value of i is: " + i);
       i++;
+    }
+
+    // for
+    for(int j =1; j<=5; j++){
+      System.out.println("Day "+j);
     }
   }
 }
