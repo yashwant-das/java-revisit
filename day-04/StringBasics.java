89fd9340767e7dce863f3cd275950536392fe3a0
Yashwant Das <yashworks@gmail.com>
2023-11-30 19:08:04 +0530

    create settings.json and StringBasics.java

---
+++ day-04/StringBasics.java
@@ -0,0 +1,8 @@
+public class StringBasics {
+    public static void main(String[] args) {
+        // String name = "Yashwant";
+        StringBuilder sb = new StringBuilder("Yashwant");
+        sb.append("das");
+        System.out.println(sb);
+    }
+}
