6104b6ff45444f49108613280ed21e8f62c4d355
Yashwant Das <yashworks@gmail.com>
2023-12-04 04:43:11 +0530

    update SwitchExpressionsExample.java

--- session-09-15/SwitchExpressionsExample.java
+++ session-09-15/SwitchExpressionsExample.java
@@ -1,28 +1,28 @@
 public class SwitchExpressionsExample {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         int dayOfWeek = 3;
 
-        String dayType = switch (dayOfWeek) {
-            case 1, 2, 3, 4, 5 -> "Weekday";
-            case 6, 7 -> "Weekend";
-            default -> "Invalid day";
-        };
+        // String dayType = switch (dayOfWeek) {
+        // case 1, 2, 3, 4, 5 -> "Weekday";
+        // case 6, 7 -> "Weekend";
+        // default -> "Invalid day";
+        // };
 
-        System.out.println("The day type is: " + dayType);
+        // System.out.println("The day type is: " + dayType);
 
-        // Using switch expression with yield (Java 16 and later)
-        String dayTypeWithYield = switch (dayOfWeek) {
-            case 1, 2, 3, 4, 5 -> {
-                yield "Weekday";
-            }
-            case 6, 7 -> {
-                yield "Weekend";
-            }
-            default -> {
-                yield "Invalid day";
-            }
-        };
+        // // Using switch expression with yield (Java 16 and later)
+        // String dayTypeWithYield = switch (dayOfWeek) {
+        // case 1, 2, 3, 4, 5 -> {
+        // yield "Weekday";
+        // }
+        // case 6, 7 -> {
+        // yield "Weekend";
+        // }
+        // default -> {
+        // yield "Invalid day";
+        // }
+        // };
 
-        System.out.println("The day type with yield is: " + dayTypeWithYield);
+        // System.out.println("The day type with yield is: " + dayTypeWithYield);
     }
 }
